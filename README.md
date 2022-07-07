85e3c8d32f90d3bbfde044aa09eedad6cd5aa330
elarchi <emilie.leury@gmail.com>
2022-07-07 18:11:50 +0100

    connection database

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Installation
 
-npm i dotenv express express-formidable mongoose
+npm i dotenv express express-formidable mongoose cors
--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 require("dotenv").config();
 
 const express = require("express");
-const formidable = require("express-formidable");
-
 const app = express();
+
+const formidable = require("express-formidable");
 app.use(formidable());
 
+const cors = require("cors");
+app.use(cors());
+
+//database connection:
+//------------------------
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI);
+
+//routes structure:
+//------------------------
 app.all("*", function (req, res) {
   res.json({ message: "Page not found" });
 });
 
+//listening to the server:
+//------------------------
 app.listen(process.env.PORT, () => {
   console.log("Server has started ✅");
 });
